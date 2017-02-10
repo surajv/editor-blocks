dea8f8db7ca2cf8c071b30761463ad8b6a61a176
iseulde <ella@iseulde.com>
2017-02-10 17:37:05 -0600

    Add some block level tools

--- js/index.js
+++ js/index.js
@@ -19,6 +19,61 @@
 			'wplink',
 			'wptextpattern'
 		],
+		blocks: {
+			paragraph: {
+				match: function( element ) {
+					return element.nodeName === 'P' && ! (
+						element.childNodes.length === 1 && element.firstChild.nodeName === 'IMG'
+					);
+				},
+				buttons: [ 'alignleft', 'aligncenter', 'alignright', 'heading', 'blockquote', 'bullist', 'preformatted' ],
+				icon: 'editor-paragraph'
+			},
+			heading: {
+				match: function( element ) {
+					var nodeName = element.nodeName;
+
+					return (
+						nodeName === 'H1' ||
+						nodeName === 'H2' ||
+						nodeName === 'H3' ||
+						nodeName === 'H4' ||
+						nodeName === 'H5' ||
+						nodeName === 'H6'
+					);
+				},
+				buttons: [ 'alignleft', 'aligncenter', 'alignright', 'heading1', 'heading2', 'heading3', 'heading4', 'heading5', 'heading6', 'removeheading' ],
+				text: 'H'
+			},
+			list: {
+				match: function( element ) {
+					return element.nodeName === 'UL' || element.nodeName === 'OL';
+				},
+				buttons: [ 'bullist', 'numlist', 'removelist' ],
+				icon: 'editor-ul'
+			},
+			image: {
+				match: function( element ) {
+					return element.childNodes.length === 1 && element.firstChild.nodeName === 'IMG';
+				},
+				buttons: [ 'alignleft', 'aligncenter', 'alignright' ],
+				icon: 'format-image'
+			},
+			blockquote: {
+				match: function( element ) {
+					return element.nodeName === 'BLOCKQUOTE';
+				},
+				buttons: [ 'removeblockquote' ],
+				icon: 'editor-quote'
+			},
+			preformatted: {
+				match: function( element ) {
+					return element.nodeName === 'PRE';
+				},
+				buttons: [ 'removepreformatted' ],
+				icon: 'editor-code'
+			}
+		},
 		schema: 'html5-strict',
 		selector: '#content',
 		theme: false,
--- js/tinymce/block.js
+++ js/tinymce/block.js
@@ -5,40 +5,116 @@
 			var element;
 			var paragraphBar;
 			var blockToolbar;
+			var blockToolbars = {};
+			var blockSelection = false;
 
-			editor.addButton( 'blocklevel', {
-				text: 'block-level tools',
-				onClick: function() {}
+			tinymce.each( '123456'.split(''), function( level ) {
+				editor.addCommand( 'heading' + level, function() {
+					editor.formatter.apply( 'h' + level );
+					editor.nodeChanged();
+				} );
+
+				editor.addButton( 'heading' + level, {
+					text: level,
+					cmd: 'heading' + level,
+					onpostrender: function() {
+						var button = this;
+
+						editor.on( 'nodechange', function( event ) {
+							button.active( element.nodeName === 'H' + level );
+						} );
+					}
+				} );
+			} );
+
+			editor.addButton( 'heading', {
+				text: 'H',
+				cmd: 'heading1'
+			} );
+
+			editor.addCommand( 'removeheading', function() {
+				editor.formatter.apply( 'p' );
+				editor.nodeChanged();
 			});
+
+			editor.addButton( 'removeheading', {
+				icon: 'dashicons dashicons-no-alt',
+				cmd: 'removeheading'
+			} );
+
+			editor.addCommand( 'preformatted', function() {
+				editor.formatter.apply( 'pre' );
+				editor.nodeChanged();
+			} );
+
+			editor.addButton( 'preformatted', {
+				icon: 'dashicons dashicons-editor-code',
+				cmd: 'preformatted'
+			} );
+
+			editor.addCommand( 'removepreformatted', function() {
+				editor.formatter.remove( 'pre' );
+				editor.nodeChanged();
+			});
+
+			editor.addButton( 'removepreformatted', {
+				icon: 'dashicons dashicons-no-alt',
+				cmd: 'removepreformatted'
+			} );
+
+			editor.addCommand( 'removeblockquote', function() {
+				editor.formatter.remove( 'blockquote' );
+				editor.nodeChanged();
+			});
+
+			editor.addButton( 'removeblockquote', {
+				icon: 'dashicons dashicons-no-alt',
+				cmd: 'removeblockquote'
+			} );
+
+			editor.addCommand( 'removelist', function() {
+				if ( element.nodeName === 'UL' ) {
+					editor.execCommand( 'InsertUnorderedList' );
+				} else if ( element.nodeName === 'OL' ) {
+					editor.execCommand( 'InsertOrderedList' );
+				}
+
+				editor.nodeChanged();
+			});
+
+			editor.addButton( 'removelist', {
+				icon: 'dashicons dashicons-no-alt',
+				cmd: 'removelist'
+			} );
 
 			editor.addButton( 'block', {
 				icon: 'dashicons dashicons-editor-paragraph',
 				tooltip: 'Add Block',
 				onClick: function() {
-					editor.selection.select( element );
-					editor.nodeChanged();
+					editor.$( element ).attr( 'data-mce-selected', 'block' );
+          editor.nodeChanged();
+
+          editor.once('click keydown', function ( event ) {
+						if ( tinymce.util.VK.modifierPressed( event ) ) {
+							return;
+						}
+
+						editor.$('*[data-mce-selected="block"]').removeAttr('data-mce-selected');
+						editor.nodeChanged();
+					} );
 				},
 				onPostRender: function() {
 					var button = this;
 
-					editor.on( 'nodechange', function(event) {
+					editor.on( 'nodechange', function( event ) {
 						element = event.parents[ event.parents.length - 1 ];
 
-						button.text( '' )
-
-						if ( event.element.nodeName === 'IMG' || editor.$( element ).find( 'img' ).length ) {
-							button.icon( 'dashicons dashicons-format-image' );
-						} else if ( element.nodeName === 'H1' ) {
-							button.icon( '' );
-							button.text( 'H1' )
-						} else if ( element.nodeName === 'H2' ) {
-							button.icon( '' );
-							button.text( 'H2' )
-						} else if ( element.nodeName === 'UL' ) {
-							button.icon( 'dashicons dashicons-editor-ul' );
-						} else {
-							button.icon( 'dashicons dashicons-editor-paragraph' );
-						}
+						tinymce.each( editor.settings.blocks, function( block, key ) {
+							if ( block.match( element ) ) {
+								button.icon( block.icon ? 'dashicons dashicons-' + block.icon : '' );
+								button.text( block.text || '' );
+							}
+						} );
 					} );
 				}
 			});
@@ -78,24 +154,26 @@
 			blockToolbar.$el.addClass('block-toolbar')
 
 			blockToolbar.reposition = function () {
-			  if (!element) return
+				if (!element) return
 
-			  var toolbar = this.getEl()
-			  var toolbarRect = toolbar.getBoundingClientRect()
-			  var elementRect = element.getBoundingClientRect()
+				var toolbar = this.getEl()
+				var toolbarRect = toolbar.getBoundingClientRect()
+				var elementRect = element.getBoundingClientRect()
 
-			  DOM.setStyles(toolbar, {
-			    position: 'absolute',
-			    left: elementRect.left - 8 - toolbarRect.width + 'px',
-			    top: elementRect.top + window.pageYOffset + 'px'
-			  })
+				var contentRect = document.getElementById( 'content' ).getBoundingClientRect();
+
+				DOM.setStyles(toolbar, {
+					position: 'absolute',
+					left: contentRect.left + 40 + 'px',
+					top: elementRect.top + window.pageYOffset + 'px'
+				} );
 
 			  this.show()
 			}
 
 			editor.on('blur', function () {
 			  blockToolbar.hide()
-			})
+			} );
 
 			editor.on( 'nodechange', function( event ) {
 				var empty = (
@@ -116,39 +194,59 @@
 
 				var range = editor.selection.getRng()
 
-				if ( ! range.collapsed && ! empty &&
-					editor.dom.isBlock( range.startContainer ) &&
-					editor.dom.isBlock( range.endContainer ) ) {
-					paragraphBar.reposition();
+				if ( editor.$( element ).attr( 'data-mce-selected' ) === 'block' ) {
+					blockSelection = true;
+
+					tinymce.each( editor.settings.blocks, function( block, key ) {
+						if ( block.match( element ) ) {
+							blockToolbars[ key ].reposition();
+						} else {
+							blockToolbars[ key ].hide();
+						}
+					} );
 				} else {
-					paragraphBar.hide();
+					blockSelection = false;
+
+					tinymce.each( editor.settings.blocks, function( block, key ) {
+						blockToolbars[ key ].hide();
+					} );
 				}
 			} )
 
-			paragraphBar = editor.wp._createToolbar( [ 'alignleft', 'aligncenter', 'alignright', 'blocklevel' ] );
+			tinymce.each( editor.settings.blocks, function( block, key ) {
+				blockToolbars[ key ] = editor.wp._createToolbar( block.buttons );
+				blockToolbars[ key ].reposition = function () {
+					if (!element) return
 
-			paragraphBar.reposition = function () {
-				if (!element) return
+					var toolbar = this.getEl()
+					var toolbarRect = toolbar.getBoundingClientRect()
+					var elementRect = element.getBoundingClientRect()
 
-				var toolbar = this.getEl()
-				var toolbarRect = toolbar.getBoundingClientRect()
-				var elementRect = element.getBoundingClientRect()
+					var contentRect = document.getElementById( 'content' ).getBoundingClientRect();
 
-				DOM.setStyles(toolbar, {
-					position: 'absolute',
-					left: elementRect.left + 'px',
-					top: elementRect.top + window.pageYOffset - toolbarRect.height - 8 + 'px'
-				})
+					DOM.setStyles(toolbar, {
+						position: 'absolute',
+						left: contentRect.left + 100 + 'px',
+						top: elementRect.top + window.pageYOffset - toolbarRect.height - 8 + 'px'
+					})
 
-				this.show()
-			}
-
-			editor.on( 'execcommand', function( event ) {
-				if ( event.command === 'JustifyLeft' || event.command === 'JustifyCenter' || event.command === 'JustifyRight' ) {
-					editor.selection.select( element );
-					editor.nodeChanged();
+					this.show()
 				}
-			} )
+			} );
+
+			editor.on( 'beforeexeccommand', function( event ) {
+				var block = blockSelection;
+
+				editor.once( 'nodechange', function( event ) {
+					setTimeout( function() {
+						if ( block ) {
+							editor.$('*[data-mce-selected="block"]').removeAttr('data-mce-selected');
+							editor.$( element ).attr( 'data-mce-selected', 'block' );
+							editor.nodeChanged();
+						}
+					} );
+				}, true );
+			} );
 		} );
 	} );
 } )( window.tinymce );
